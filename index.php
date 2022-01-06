ac70c474c800712b8d3879843730e797a2effb30
luqaska <83518257+luqaska@users.noreply.github.com>
2022-01-06 02:48:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header("content-type: text/plain");
+if(isset($_POST["key"]) && isset($_POST["file"])){
+if($_POST["key"]=="key"){
+if($_POST["file"]=="" || $_POST["file"]=="index.html" || $_POST["file"]=="index.php"){
+include "files/index.php";
+die("text/html\n".$html);
+}elseif(file_exists("files/".$_POST["file"])){
+include "files/index.php";
+die($mime."\n".$html);
+}else{die("text/html\nError 404");}
+}
+} ?>
