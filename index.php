ec5fa2efbb7abd5090d556880cbc95e110ca6a07
luqaska <83518257+luqaska@users.noreply.github.com>
2022-01-06 02:49:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ if(isset($_POST["key"]) && isset($_POST["file"])){
 if($_POST["key"]=="key"){
 if($_POST["file"]=="" || $_POST["file"]=="index.html" || $_POST["file"]=="index.php"){
 include "files/index.php";
-die("text/html\n".$html);
+die("text/markdown\n".$html);
 }elseif(file_exists("files/".$_POST["file"])){
 include "files/index.php";
 die($mime."\n".$html);
-}else{die("text/html\nError 404");}
+}else{die("text/markdown\n#Error 404");}
 }
 } ?>
